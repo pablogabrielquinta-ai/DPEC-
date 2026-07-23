833883b2baf5207b7d7df05d124a1f9faec93cbe
pablogabrielquinta-ai <pablogabrielquinta@gmail.com>
2026-07-23 15:31:17 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Así las actualizaciones del repo llegan siempre,
    pero la app abre igual sin señal. */
 
-var CACHE = 'dpec-foto-medidor-v8';
+var CACHE = 'dpec-foto-medidor-v9';
 var ARCHIVOS = [
   './dpec_foto_medidor.html',
   './manifest.json',
