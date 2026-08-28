e3ae476ba0b4c5ec19218407aa6e9d78f945d822
pablogabrielquinta-ai <pablogabrielquinta@gmail.com>
2026-08-28 14:50:24 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Estrategia: caché primero (abre al toque), actualiza en segundo
    plano. La próxima vez que se abra ya está la versión nueva. */
 
-var CACHE = 'dpec-foto-medidor-v27';
+var CACHE = 'dpec-foto-medidor-v28';
 var ARCHIVOS = [
   './dpec_foto_medidor.html',
   './manifest.json',
